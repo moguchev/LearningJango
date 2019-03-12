760243a8138f3a9a3898328adbf01842cc6bf8a9
moguchev <abracadabra.14@mail.ru>
2019-03-13 00:15:19 +0300

    Создать исходное приложение Django

--- LearningDjango/LearningDjango/settings.py
+++ LearningDjango/LearningDjango/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app',
+    'HelloDjangoApp',
     # Add your apps here to enable them
     'django.contrib.admin',
     'django.contrib.auth',
