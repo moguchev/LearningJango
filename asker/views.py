28e695fe83a100d47b5f6fc08d0918865f762269
moguchev <abracadabra.14@mail.ru>
2019-03-28 10:36:43 +0200

    1

--- asker/views.py
+++ asker/views.py
@@ -2,29 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request) :
+def index(request):
     questions = [
-        { 'id' : i, 'title' : f'Question #{i}'}
+        {'id': i, 'title': f'Question #{i}', 'tags': ['bender', 'django'],
+         'text': f'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.'}
         for i in range(3)
     ]
     return render(request, 'index.html', {
-        'questions' : questions,
+        'questions': questions,
     })
 
-def login(request) :
+
+def login(request):
     return render(request, 'login.html', {})
 
-def ask(request) :
+
+def ask(request):
     return render(request, 'ask.html', {})
 
-def register(request) :
+
+def register(request):
     return render(request, 'signup.html', {})
 
-def question(request, id) :
+
+def question(request, id):
     return render(request, 'question.html', {})
 
-def settings(request) :
+
+def settings(request):
     return render(request, 'settings.html', {})
 
-def tag(request) :
-    return render(request, 'tag.html', {})
+
+def tag(request, tag):
+    questions = [
+        {'id': i, 'title': f'Question #{i}', 'tags': [tag],
+         'text': f'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.'}
+        for i in range(3)
+    ]
+    return render(request, 'tag.html', {
+        'questions': questions,
+        'tag': tag,
+    })
