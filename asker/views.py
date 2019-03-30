9a7ef04bb5c1443218de92dc08563a25214d059e
moguchev <abracadabra.14@mail.ru>
2019-03-30 21:50:23 +0200

    faker

--- asker/views.py
+++ asker/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from faker import Faker
 
+fake = Faker()
 
 def index(request):
     questions = [
-        {'id': i, 'title': f'Question #{i}', 'tags': ['bender', 'django'],
-         'text': f'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.'}
-        for i in range(3)
+        {
+            'id': i,
+            'title': fake.sentence(),
+            'text' : '\n'.join(fake.sentences(fake.random_int(3, 6))),
+            'tags' : [fake.word() for i in range(fake.random_int(2, 5))],
+        }
+        for i in range(5)
     ]
     return render(request, 'index.html', {
         'questions': questions,
@@ -35,9 +41,13 @@ def settings(request):
 
 def tag(request, tag):
     questions = [
-        {'id': i, 'title': f'Question #{i}', 'tags': [tag],
-         'text': f'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.'}
-        for i in range(3)
+        {
+            'id': i,
+            'title': fake.sentence(),
+            'text': '\n'.join(fake.sentences(fake.random_int(3, 6))),
+            'tags': [tag]
+        }
+        for i in range(5)
     ]
     return render(request, 'tag.html', {
         'questions': questions,
